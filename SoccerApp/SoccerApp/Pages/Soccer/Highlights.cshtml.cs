c176e3f26908b48e730bd6e0a0998ba82d191770
Biniam <Biniamt23@yahoo.com>
2020-02-04 17:46:07 -0800

    highlights tab and videos added

---
+++ SoccerApp/SoccerApp/Pages/Soccer/Highlights.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SoccerApp
+{
+    public class HighlightsModel : PageModel
+    {
+        public string Message { get; set; }
+        public void OnGet()
+        {
+            Message = "View Highlights of the most recent soccer matches!";
+        }
+    }
+}
